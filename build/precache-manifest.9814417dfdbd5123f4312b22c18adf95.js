fde1e7af745d85e2cf982f561d24096331060f3a
Akash Sharma <akash1991@gmail.com>
2022-07-29 04:52:10 +0530

    deploy again

---
+++ build/precache-manifest.9814417dfdbd5123f4312b22c18adf95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "86551f3890d02b8bc2ba",
+    "url": "/ipod/static/css/main.41f78cfc.chunk.css"
+  },
+  {
+    "revision": "86551f3890d02b8bc2ba",
+    "url": "/ipod/static/js/main.86551f38.chunk.js"
+  },
+  {
+    "revision": "4319e8b4e94411c764ab",
+    "url": "/ipod/static/js/runtime~main.4319e8b4.js"
+  },
+  {
+    "revision": "9702c452370a8f539e98",
+    "url": "/ipod/static/js/2.9702c452.chunk.js"
+  },
+  {
+    "revision": "6c29fb468518f7268e0852fbab5cbf0c",
+    "url": "/ipod/static/media/games.6c29fb46.png"
+  },
+  {
+    "revision": "a3d9f9e7ef385c3a63db63c7967bb06f",
+    "url": "/ipod/static/media/settings.a3d9f9e7.png"
+  },
+  {
+    "revision": "89583f2d774ef9f948a41cb4dfbfccfd",
+    "url": "/ipod/static/media/cover.89583f2d.png"
+  },
+  {
+    "revision": "b1de0421f1d558bae003fc8714bf53b3",
+    "url": "/ipod/static/media/music.b1de0421.png"
+  },
+  {
+    "revision": "b2c485c12c22a2862b2db8fa67c598a8",
+    "url": "/ipod/static/media/ipod.b2c485c1.png"
+  },
+  {
+    "revision": "c2a2f90fd895232c9759469ffbbe6e48",
+    "url": "/ipod/static/media/ipod_bg.c2a2f90f.jpeg"
+  },
+  {
+    "revision": "dd8f79686e46b1f88e7859b55d8ce6c7",
+    "url": "/ipod/index.html"
+  }
+];
